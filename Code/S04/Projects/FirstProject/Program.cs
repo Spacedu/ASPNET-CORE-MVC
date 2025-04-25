c23b508002c20ee6a4a27fdde8b100b6026091fc
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-04-24 23:46:35 -0300

    S04E04 - Analisando nossa resposta

--- Code/S04/Projects/FirstProject/Program.cs
+++ Code/S04/Projects/FirstProject/Program.cs
@@ -2,7 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Site under construction");
+
+    if (context.Request.Path.StartsWithSegments("/contact"))
+    {
+        context.Response.Headers.ContentType = "text/html";
+
+        await context.Response.WriteAsync("<h1>Contact page</h1>with form!");
+    }
+    else { 
+        await context.Response.WriteAsync("Site under construction");
+    }
 });
 
 app.Run();
