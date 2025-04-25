b7763e63caa6f4deb6dc38d51ca7f9a09fec83ca
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-04-24 23:53:07 -0300

    S04E05 - Criando nossa primeira API

---
+++ Code/S04/Projects/FirstProject/Client.cs
@@ -0,0 +1,7 @@
+﻿namespace FirstProject
+{
+    public class Client
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- Code/S04/Projects/FirstProject/Program.cs
+++ Code/S04/Projects/FirstProject/Program.cs
@@ -1,15 +1,30 @@
+using System.Text.Json;
+using FirstProject;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Run(async (HttpContext context) => {
+List<Client> clients = new List<Client>();
+clients.Add(new Client { Name = "John" });
+clients.Add(new Client { Name = "Joseph" });
+clients.Add(new Client { Name = "Mary" });
 
-    if (context.Request.Path.StartsWithSegments("/contact"))
+app.Run(async (HttpContext context) => {
+    if (context.Request.Path.StartsWithSegments("/api/clients"))
+    {
+        context.Response.Headers.ContentType = "application/json";
+
+        var json = JsonSerializer.Serialize(clients);
+        await context.Response.WriteAsync(json);
+
+    } else if (context.Request.Path.StartsWithSegments("/contact"))
     {
         context.Response.Headers.ContentType = "text/html";
 
         await context.Response.WriteAsync("<h1>Contact page</h1>with form!");
     }
-    else { 
+    else
+    {
         await context.Response.WriteAsync("Site under construction");
     }
 });
