e3aea46f04356d6ccdee43568a024f91433c2ceb
Elias Ribeiro (Spacedu) <contato@spacedu.com.br>
2025-04-26 15:10:33 -0300

    S06E02 - Model Binding - Parte 1

--- Code/S04&S05/Projects/SecondProject/Controllers/NewsController.cs
+++ Code/S04&S05/Projects/SecondProject/Controllers/NewsController.cs
@@ -11,13 +11,23 @@ public class NewsController : Controller
         return Ok("News > List");
     }
 
+    /*
+     * Model Binding
+     * 
+     * HTML Form = Method = GET | POST
+     * ?title=xyz&message=xyz (80k char limit)
+     * 
+     * Form: title=xyz&message=xyz
+     */
+    [HttpPost("Add")]
+    public IActionResult Add([FromQuery]string author, string title, string message)
+    {
+        return Ok();
+    }
+
     [HttpGet("Edit/{id}")]
     public IActionResult Edit(int id)
     {
         return Ok($"News > Edit: {id}");
     }
-
-    
-
-
 }
