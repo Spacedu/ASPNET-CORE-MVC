b31728c190c9d8bd859944c63b26d49d57d2c750
Elias Ribeiro (Spacedu) <contato@spacedu.com.br>
2025-04-26 12:23:26 -0300

    S05E03 - Configurar o MVC

--- Code/S04&S05/Projects/SecondProject/Program.cs
+++ Code/S04&S05/Projects/SecondProject/Program.cs
@@ -1,7 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+#region MVC - Service
+builder.Services.AddControllersWithViews();
+#endregion
 
+var app = builder.Build();
 //Middleware - Blazor, MVC, RazorPage
 app.UseStaticFiles();
 
+#region MVC - Config. Routing
+/*
+ * MVC - URL
+ * https://localhost/{controller}/{action}/{id?}
+ * https://localhost/Product/Index
+ * https://localhost/Product/Add
+ * https://localhost/Product/Edit/1
+ * https://localhost/Product/Delete/1
+ * https://localhost:7000/
+ * 
+ * https://localhost/Product > https://localhost/Product/Index
+ */
+
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+#endregion
 app.Run();
