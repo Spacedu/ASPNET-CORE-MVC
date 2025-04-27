fb1161ff60f0feacc78bb5c7680def5f16217947
Elias Ribeiro (Spacedu) <contato@spacedu.com.br>
2025-04-27 12:00:46 -0300

    S06E05 - Produzindo Resultado - Content

---
+++ Code/S04&S05/Projects/SecondProject/Controllers/ResultTypeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecondProject.Controllers;
+
+[Route("ResultType")]
+public class ResultTypeController : Controller
+{
+    [Route("Content")]
+    public IActionResult ContentResult()
+    {
+        return Content("This is content result 2", "text/plain");
+
+        return new ContentResult
+        {
+            StatusCode = 200,
+            ContentType = "text/plain",
+            Content = "This is content result"
+        };
+    }
+}
