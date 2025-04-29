03656d8eafef547be49641d6ca93819ea21371fb
Elias Ribeiro (Spacedu) <contato@spacedu.com.br>
2025-04-29 16:36:55 -0300

    S07E04 - Razor - Passando Dados - ViewData, ViewBag e TempData - Parte 2

--- Code/S04&S05/Projects/SecondProject/Controllers/PageController.cs
+++ Code/S04&S05/Projects/SecondProject/Controllers/PageController.cs
@@ -12,8 +12,40 @@ namespace SecondProject.Controllers
 
         public IActionResult PassData()
         {
+            /*
+             * Request/Response
+             * ViewBag - Dynamic
+             * ViewData - Dictonary
+             * 
+             * Request/Response > Redirect
+             * TempData - Dictonary
+             */
+
+            ViewBag.Company = "Microsoft";
+            ViewBag.Founder = "Bill Gates";
+            ViewBag.CEO = "Satya Nadella";
+
+            ViewData["Company2"] = "Apple";
+            ViewData["Founder2"] = "Steven Jobs & Steve Wozniak";
+            ViewData["CEO2"] = "Tim Cook";
+
+
+            TempData["Company"] = "Oracle";
+            TempData["Founder"] = "Larry Ellison";
+            TempData["CEO"] = "Safra A. Catz";
+
+            /*
+             * For Test TempData enable this line: return RedirectToAction("ViewTempData");
+             */
+            
+
             var news = new News { Title = "News of now", Author="Elias", Message="Good day!" };
             return View(news);
+        }
+
+        public IActionResult ViewTempData()
+        {
+            return Ok($"{TempData.Peek("Company")} - {TempData["Founder"]} - {TempData["CEO"]}");
         }
     }
 }
