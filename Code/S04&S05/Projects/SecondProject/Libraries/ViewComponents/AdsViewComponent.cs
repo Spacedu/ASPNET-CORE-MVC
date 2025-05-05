71ce815ca40bce7a9b1203390393421872848181
Elias Ribeiro (Spacedu) <contato@spacedu.com.br>
2025-05-05 14:09:09 -0300

    S08E05 - Razor - View Components - Parte 1

---
+++ Code/S04&S05/Projects/SecondProject/Libraries/ViewComponents/AdsViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecondProject.Libraries.ViewComponents;
+
+public class AdsViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View();
+    }
+}
