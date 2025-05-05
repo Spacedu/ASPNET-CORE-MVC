6f8efdab91787c28924aab03e7c02c023e2ab1ed
Elias Ribeiro (Spacedu) <contato@spacedu.com.br>
2025-05-05 14:19:54 -0300

    S08E05 - Razor - View Components - Parte 2

--- Code/S04&S05/Projects/SecondProject/Libraries/ViewComponents/AdsViewComponent.cs
+++ Code/S04&S05/Projects/SecondProject/Libraries/ViewComponents/AdsViewComponent.cs
@@ -4,8 +4,25 @@ namespace SecondProject.Libraries.ViewComponents;
 
 public class AdsViewComponent : ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync()
+    public async Task<IViewComponentResult> InvokeAsync(string title)
     {
-        return View();
+        //TODO - Get List from Database
+        var ads = GetAdsFromDatabase();
+        return View(ads);
     }
+
+    public List<Ads> GetAdsFromDatabase()
+    {
+        List<Ads> ads = new List<Ads>();
+        ads.Add(new Ads() { Title = "Ads 1", Description = "Lorem 1....." });
+        ads.Add(new Ads() { Title = "Ads 2", Description = "Lorem 2....." });
+        ads.Add(new Ads() { Title = "Ads 3", Description = "Lorem 3....." });
+
+        return ads;
+    }
+}
+public class Ads
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
 }
